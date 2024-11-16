100f47ccef3f5924ef223fe839f3142f2c9bfe85
phanquangduong2002 <phanquangduong2002@gmail.com>
2024-11-16 14:34:49 +0700

    fix

--- server/app/Services/UploadService.php
+++ server/app/Services/UploadService.php
@@ -26,7 +26,7 @@ class UploadService
 
             $s3 = new S3Client([
                 'version' => 'latest',
-                'region'  => 'ap-southeast-1',
+                'region'  => env('AWS_DEFAULT_REGION'),
                 'credentials' => [
                     'key'    => env('AWS_ACCESS_KEY_ID'),
                     'secret' => env('AWS_SECRET_ACCESS_KEY'),
