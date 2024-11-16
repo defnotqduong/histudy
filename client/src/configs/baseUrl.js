08ebe6abcc39932d6bc1752f4c75851be81acd80
phanquangduong2002 <phanquangduong2002@gmail.com>
2024-11-16 21:17:46 +0700

    fix

--- client/src/configs/baseUrl.js
+++ client/src/configs/baseUrl.js
@@ -2,4 +2,4 @@ const isProd = import.meta.env.PROD
 
 export const BASE_API_URL = isProd ? 'http://18.139.108.134:8000/api' : 'http://18.139.108.134:8000/api'
 
-export const BASE_WS_URL = isProd ? 'ws://localhost:8080' : 'ws://localhost:8080'
+export const BASE_WS_URL = isProd ? 'ws://18.139.108.134:8080' : 'ws://18.139.108.134:8080'
