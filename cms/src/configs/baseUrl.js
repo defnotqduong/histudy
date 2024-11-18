dee046bfe805a85383f30cc15b7b1348e3ae385b
phanquangduong2002 <phanquangduong2002@gmail.com>
2024-11-18 09:46:47 +0700

    fix

--- cms/src/configs/baseUrl.js
+++ cms/src/configs/baseUrl.js
@@ -1,5 +1,5 @@
 const isProd = import.meta.env.PROD
 
-export const BASE_API_URL = isProd ? 'http://18.139.108.134:8000/api' : 'http://18.139.108.134:8000/api'
+export const BASE_API_URL = isProd ? 'https://www.api.histudy.online/api' : 'https://www.api.histudy.online/api'
 
 export const BASE_WS_URL = isProd ? 'ws://18.139.108.134:8080' : 'ws://18.139.108.134:8080'
