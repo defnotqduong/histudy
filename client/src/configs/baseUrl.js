a8b396fe74e913add7729a30eeb591691d0df38c
phanquangduong2002 <phanquangduong2002@gmail.com>
2024-11-18 11:36:54 +0700

    fix

--- client/src/configs/baseUrl.js
+++ client/src/configs/baseUrl.js
@@ -2,4 +2,4 @@ const isProd = import.meta.env.PROD
 
 export const BASE_API_URL = isProd ? 'https://www.api.histudy.online/api' : 'https://www.api.histudy.online/api'
 
-export const BASE_WS_URL = isProd ? 'wss://sm.histudy.online' : 'wss://sm.histudy.online'
+export const BASE_WS_URL = isProd ? 'wss://www.sm.histudy.online' : 'wss://www.sm.histudy.online'
