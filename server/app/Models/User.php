e1cd5e1e1174288eec5d9692dd5e1dddd38120e2
phanquangduong2002 <phanquangduong2002@gmail.com>
2024-11-30 18:13:06 +0700

    fix

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -77,7 +77,7 @@ class User extends Authenticatable implements JWTSubject
 
     public function certificates()
     {
-        return $this->belongsToMany(Certificate::class, 'user_id');
+        return $this->belongsToMany(Certificate::class,);
     }
 
     public function cart()
